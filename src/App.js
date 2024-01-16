ba1d2d103614ce5067e3c11e955eff1d6ebc3ae1
Ankita <ankitasinghr45@gmail.com>
2024-01-16 21:31:46 +0530

    hooks#2

--- src/App.js
+++ src/App.js
@@ -4,26 +4,33 @@ import './index.css'
 import Slot from './Slot';
 import { useState } from 'react';
 
-const App =()=>{
+const App = () => {
 
-  const [num,incNum] = useState(10);
-  const count =()=>{
-    if(num>0){
-    incNum(num-1);
+  const [num, incNum] = useState(10);
+  const count = () => {
+    if (num > 0) {
+      incNum(num - 1);
     }
   }
-  return(
+  var nowTime = new Date().toLocaleTimeString();
+  const [currentTime, updateTime] = useState(nowTime);
+  const time = () => {
+    updateTime(new Date().toLocaleTimeString())
+  }
+  return (
     <>
-    <h1 className='heading_style'> 🎰 Welcome To Slot Machine Game 🎰</h1>
-    <div className='slotMachine'>
-      <Slot x="❤️" y="❤️" z="❤️" />
-      <Slot x="🤑" y="😎" z="😋" />
-      <Slot x="❤️" y="❤️" z="😊" />
-      <Slot x="👻" y="👻" z="👻" />
-      <Slot x="😴" y="🐟" z="🍔" />
+      <h1 className='heading_style'> 🎰 Welcome To Slot Machine Game 🎰</h1>
+      <div className='slotMachine'>
+        <Slot x="❤️" y="❤️" z="❤️" />
+        <Slot x="🤑" y="😎" z="😋" />
+        <Slot x="❤️" y="❤️" z="😊" />
+        <Slot x="👻" y="👻" z="👻" />
+        <Slot x="😴" y="🐟" z="🍔" />
       </div>
       <h1 className='number'>{num}</h1>
       <button onClick={count}>Click Me</button>
+      <h1 className='number'>{currentTime}</h1>
+      <button onClick={time}>Show Time</button>
     </>
   )
 }
